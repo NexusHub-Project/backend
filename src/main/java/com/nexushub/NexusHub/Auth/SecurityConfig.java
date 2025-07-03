d647b5ae44b65d1c80d0b1cda835614c5328617f
Sua Lee (Hannah) <peter48@inu.ac.kr>
2025-07-03 16:02:05 +0900

    swagger 설정

--- src/main/java/com/nexushub/NexusHub/Auth/SecurityConfig.java
+++ src/main/java/com/nexushub/NexusHub/Auth/SecurityConfig.java
@@ -33,7 +33,9 @@ public class SecurityConfig {
                                 "/import/**",
                                 "/api/v1/patchnote/test", "/api/v1/patchnote/find/*", "/api/v1/patchnote/edit/*", "/api/v1/patchnote/delete/*", "/api/v1/patchnote/show/**",
                                 "/api/v1/comment/**",
-                                "/api/v1/strategy/**"
+                                "/api/v1/strategy/**",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
                         ).permitAll()
                         .anyRequest().authenticated()
 //
